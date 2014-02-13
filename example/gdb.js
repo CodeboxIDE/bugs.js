c06bc3e450ca2042e8fc6bb839d963bf9c5253c1
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-02-13 02:08:44 +0100

    Add gdb example

---
+++ example/gdb.js
@@ -0,0 +1,22 @@
+// Requires
+var bugs = require('../');
+
+// Use gdb to debug the unix "ls" binary
+var dbg = bugs.gdb('ls');
+
+// Debug "main" function
+dbg.break('main')
+.then(function() {
+    // Run "ls" on a given folder
+    return dbg.run('-al /tmp');
+})
+.then(function() {
+    // Get backtrace
+    return dbg.backtrace();
+})
+.then(function(trace) {
+    // Display trace & quit
+    console.log('trace =', trace);
+    return dbg.quit();
+})
+.done();
