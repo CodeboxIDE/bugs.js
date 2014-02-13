5a1c02706dbd992c1c899e850e7c58769cf38c23
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-02-13 12:57:15 +0100

    Add simple python test program

---
+++ test/hello.py
@@ -0,0 +1,20 @@
+def unused():
+    pass
+
+
+def myf(x):
+    a = 'ccc'
+
+    d = 99*5
+
+    unused()
+
+    return 99*x+3
+
+
+def main():
+    x = 34
+    print(myf(7))
+    print(myf(myf(9)))
+
+main()
