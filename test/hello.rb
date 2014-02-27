3164f38b64e1b9300f234d1e52b21c2d80120ef0
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-02-27 23:59:39 +0100

    Add hello world ruby test

---
+++ test/hello.rb
@@ -0,0 +1,23 @@
+location = {
+  :home => "france",
+  :work => "california",
+  :addr => {
+    :town => "Palo Alto",
+    :street => "Something"
+  }
+}
+
+name = 'aaron'
+age = 20
+
+def f(y, z)
+  y * z * 3
+end
+
+f(88, 2)
+
+if f(3, 1) < 4
+  puts "good"
+else
+  puts "bad"
+end
