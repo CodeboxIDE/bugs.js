d5ab60e49d6010aca4c502cbbf260982287f1a33
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-03-01 21:57:59 +0100

    Add class to test/hello.rb

--- test/hello.rb
+++ test/hello.rb
@@ -7,8 +7,20 @@ location = {
   }
 }
 
+class Person
+    def initialize(name, age)
+        @age = age
+        @name = name
+    end
+
+    def hello
+        puts "Hello my name is #{@name}"
+    end
+end
+
 name = 'aaron'
 age = 20
+aaron = Person.new(name, age)
 
 def f(y, z)
   y * z * 3
@@ -20,4 +32,10 @@ if f(3, 1) < 4
   puts "good"
 else
   puts "bad"
+end
+
+if aaron.age < 18
+    puts "young"
+else
+    puts "old "
 end
